402626a7d1c9dbe2cfc2c7d5f90acbb921680dcd
Vineshgvk <vinesh.gandhe@gmail.com>
2021-11-30 18:14:37 +0530

    added jquery in js file

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+//    Adding JQuery
+var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js";
+// Then bind the event to the callback function.
+// There are several events for cross browser compatibility.
+script.onreadystatechange = handler;
+script.onload = handler;
+// Fire the loading
+head.appendChild(script);
 
+function handler() {
+    console.log('jquery added :)');
+}
 
 var config={kcAllowedFuncNames:["zendeskchat"]};
 
