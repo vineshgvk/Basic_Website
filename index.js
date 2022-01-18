ce22e4bcbc7d0f1021597fa4269ffbdaeca68c73
vineshgvk <vinesh.gandhe@gmail.com>
2022-01-18 20:19:42 +0530

    reverted the changes from the static folder

--- index.js
+++ index.js
@@ -36,47 +36,47 @@ function zendeskchat(){
 }
 
 //PX tag configuration
-//   (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
-//       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
-//     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
-//     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-//   })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-K0EYK7ZWBNRU-2",config);
+  (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
+    var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
+    var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-K0EYK7ZWBNRU-2",config);
 
 //testing the static file load of aptrinsic.js file
 
-(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
-    (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
-  var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
-  var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-})(window,document,"https://vineshgvk.github.io/Basic_Website/aptrinsic.js","AP-K0EYK7ZWBNRU-2",config);
+// (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+//     (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
+//   var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
+//   var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
+// })(window,document,"https://vineshgvk.github.io/Basic_Website/aptrinsic.js","AP-K0EYK7ZWBNRU-2",config);
 
 // window.location = "https://vineshgvk.github.io/Basic_Website/aptrinsic.js";
 
 //   iframe tag configure---start
 
-// function initializeGainsightPX() 
-// {
-//     if (this.isGainsightPXInitialized) return;
+function initializeGainsightPX() 
+{
+    if (this.isGainsightPXInitialized) return;
     
-//     // Gainsight PX Tag
-//     (function (n, t, a, e, x) {   
-//         let i = "aptrinsic"; n[i] = n[i] || function () {
-//             (n[i].q = n[i].q || []).push(arguments)
-//         }, 
-//         n[i].p = e, n[i].c = x;
+    // Gainsight PX Tag
+    (function (n, t, a, e, x) {   
+        let i = "aptrinsic"; n[i] = n[i] || function () {
+            (n[i].q = n[i].q || []).push(arguments)
+        }, 
+        n[i].p = e, n[i].c = x;
         
-//         let r = t.createElement("script");
-//         r.async = !0;
-//         r.src = a + "?a=" + e;        
+        let r = t.createElement("script");
+        r.async = !0;
+        r.src = a + "?a=" + e;        
         
-//         let c = t.getElementsByTagName("script")[0];
-//         c.parentNode.insertBefore(r, c);
-//     })
-//     (window, document, "https://web-sdk.aptrinsic.com/api/aptrinsic.js", "AP-K0EYK7ZWBNRU-2",{ "iframeModeEnabled": false });
-//     // End Gainsight PX Tag    
+        let c = t.getElementsByTagName("script")[0];
+        c.parentNode.insertBefore(r, c);
+    })
+    (window, document, "https://web-sdk.aptrinsic.com/api/aptrinsic.js", "AP-K0EYK7ZWBNRU-2",{ "iframeModeEnabled": false });
+    // End Gainsight PX Tag    
     
-//     this.isGainsightPXInitialized = true;
-// }
+    this.isGainsightPXInitialized = true;
+}
 
 
 //   iframe tag configure---end
@@ -304,4 +304,4 @@ function transactiondone(ev1) {
 
 
 
-//iframe tag commented
+
