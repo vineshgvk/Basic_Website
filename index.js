75be6b97c5ce4c8278548a92dc5944921dad5f7f
vineshgvk <vinesh.gandhe@gmail.com>
2022-01-18 19:59:27 +0530

    edited the static file path in the px tag

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function zendeskchat(){
     (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
   var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
   var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-})(window,document,window.location = "https://vineshgvk.github.io/Basic_Website/aptrinsic.js","AP-K0EYK7ZWBNRU-2",config);
+})(window,document,"https://vineshgvk.github.io/Basic_Website/aptrinsic.js","AP-K0EYK7ZWBNRU-2",config);
 
 // window.location = "https://vineshgvk.github.io/Basic_Website/aptrinsic.js";
 
