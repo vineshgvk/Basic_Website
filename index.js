b5b0b54f50f7dd538ca88176a80a8e7721363ee4
vineshgvk <67590588+vineshgvk@users.noreply.github.com>
2022-12-02 14:10:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ var config={kcAllowedFuncNames:["zendeskchat"],"secureCookie":true};
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://vineshgvk.github.io/Basic_Website/aptrinsic.js","AP-K0EYK7ZWBNRU-2", {
+  })(window,document,"https://vineshgvk.github.io/Basic_Website/aptrinsic.js","AP-VERVA8EO9OIT-2", {
     
    
     "engagementChecksumFileUrl": "https://vineshgvk.github.io/Basic_Website/Engagement_checksum_First product_Production.json"
